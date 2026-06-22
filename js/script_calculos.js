2f76f0686dd274843bd3b17fdc9c9dfccd531947
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-06-22 16:12:43 -0300

    calculo renda

---
+++ js/script_calculos.js
@@ -0,0 +1,17 @@
+const calDesconto = (objPessoa ) => {
+    let resultCalc = ''
+
+    if((objPessoa.idade < 18) || (objPessoa.renda <= 1700)){
+        resultCalc = 'Isento de desconto'
+    }else if(objPessoa.renda <= 5000){
+        resultCalc = objPessoa.renda * 0.05
+    }else if(objPessoa.renda <= 8000){
+        resultCalc = objPessoa.renda * 0.10
+    }else if(objPessoa.renda <= 12000){
+        resultCalc = objPessoa.renda * 0.15
+    }else{
+        resultCalc = objPessoa.renda * 0.20
+    } 
+    
+    return resultCalc
+}
